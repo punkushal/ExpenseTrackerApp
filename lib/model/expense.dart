1652dd140b88e4e4fa701a189acc8551ea86cbf8
punkushal <spycymagar@gmail.com>
2023-09-18 13:43:26 +0545

    Introduced Enums

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -5,6 +5,9 @@ import 'package:uuid/uuid.dart';
 //This is a utility class object to generate unique ids
 const uuid = Uuid();
 
+//This Category is custom type
+enum Category { food, travel, leisure, work }
+
 class Expense {
   final String title;
   final double amount;
@@ -12,6 +15,11 @@ class Expense {
   //Here we'll generating id dynamically
   //whenever this class object is instantiated
   final String id;
-  Expense({required this.title, required this.amount, required this.date})
+  final Category category;
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
       : id = uuid.v4(); //This V4() will generate unique string id
 }
