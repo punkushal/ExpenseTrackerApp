50814c0f3377d7fdee5796deeadb5b568edbcd4c
punkushal <spycymagar@gmail.com>
2023-09-18 13:52:53 +0545

    Creating Dummy Data

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 //This file is used to display and handle user interface
+import 'package:expense_tracker_app/model/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatelessWidget {
@@ -6,6 +7,18 @@ class Expenses extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Expense> _registeredExpenses = [
+      Expense(
+          title: 'Flutter Course',
+          amount: 12000,
+          date: DateTime.now(),
+          category: Category.work),
+      Expense(
+          title: 'Cinema',
+          amount: 2000,
+          date: DateTime.now(),
+          category: Category.leisure),
+    ];
     return const Scaffold(
       body: Column(
         children: [
