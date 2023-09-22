685044ee03f5d5e062baec9ff1f6f2cf0480049a
punkushal <spycymagar@gmail.com>
2023-09-22 21:37:48 +0545

    Setting & Using A Color Scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:expense_tracker_app/widget/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith(useMaterial3: true),
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          )),
       home: const Expenses(),
     ),
   );
